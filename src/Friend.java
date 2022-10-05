718e5c8bb00d31c5efffbdc8ba2b9ef98c622962
Oliver Liljeblad <liljeblad.oliver@elev.ga.ntig.se>
2022-10-05 14:32:49 +0200

    first

--- src/Friend.java
+++ src/Friend.java
@@ -1,2 +1,4 @@
-package PACKAGE_NAME;public class Friend {
+public class Friend {
+    public Friend() {}
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println();
+        Scanner myScan= new Scanner(System.in);
+        System.out.println("write a number");
+        int hej= myScan.nextInt();
+        String hopp = myScan.next();
     }
 }
